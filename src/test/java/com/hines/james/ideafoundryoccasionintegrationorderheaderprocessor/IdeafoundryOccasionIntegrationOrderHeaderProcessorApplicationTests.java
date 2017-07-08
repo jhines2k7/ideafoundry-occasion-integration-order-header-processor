8b954f334d63386d34f208c6ac4565c9bd1007cc
James Hines <jhines2k7@yahoo.com>
2017-07-08 07:24:58 -0400

    Adds import for @Ignore annotation

--- src/test/java/com/hines/james/ideafoundryoccasionintegrationorderheaderprocessor/IdeafoundryOccasionIntegrationOrderHeaderProcessorApplicationTests.java
+++ src/test/java/com/hines/james/ideafoundryoccasionintegrationorderheaderprocessor/IdeafoundryOccasionIntegrationOrderHeaderProcessorApplicationTests.java
@@ -1,6 +1,7 @@
 package com.hines.james.ideafoundryoccasionintegrationorderheaderprocessor;
 
 import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
